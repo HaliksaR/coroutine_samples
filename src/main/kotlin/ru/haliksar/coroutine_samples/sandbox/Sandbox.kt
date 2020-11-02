e5fa4093ae0336a0490d52d3857fd83fc82e6fad
HaliksaR <fun.haliksar.devel@gmail.com>
2020-11-03 00:58:22 +0700

    https://github.com/Kotlin/kotlinx.coroutines/issues/410

--- src/main/kotlin/ru/haliksar/coroutine_samples/sandbox/Sandbox.kt
+++ src/main/kotlin/ru/haliksar/coroutine_samples/sandbox/Sandbox.kt
@@ -1,54 +1,51 @@
 package ru.haliksar.coroutine_samples.sandbox
 
 import kotlinx.coroutines.*
-import kotlin.coroutines.CoroutineContext
 
 
-class UseCase {
+class Scenario(
+    private val usecase1: Usecase1,
+    private val usecase2: Usecase2,
+) {
 
-    private var scopeJob: CoroutineScope? = null
-
-    companion object {
-        val instance = UseCase()
-    }
-
-    suspend operator fun invoke(
-        handler: CoroutineExceptionHandler = CoroutineExceptionHandler { _: CoroutineContext, _: Throwable -> }
-    ) {
-        scopeJob = CoroutineScope(Job() + handler)
-        scopeJob?.launch {
+    suspend operator fun invoke() {
+        coroutineScope {
             listOf(
-                async {
-                    println("start1")
-                    delay(100)
-                    throw Exception("поймай")
-                    println("end1")
-                },
-                async {
-                    println("start2")
-                    delay(1000)
-                    println("end2")
-                },
-            ).forEach { it.await() }
-        }?.join()
+                async { usecase1() },
+                async { usecase2() },
+            ).awaitAll()
+        }
     }
-
-    fun cancel() = scopeJob?.coroutineContext?.cancelChildren()
 }
 
-private val handler = CoroutineExceptionHandler { coroutineContext, throwable ->
-    println(throwable.message)
+class Usecase1 {
+    suspend operator fun invoke() {
+        println("start1")
+        delay(100)
+//        throw Exception("поймай")
+        println("end1")
+    }
+}
+
+class Usecase2 {
+    suspend operator fun invoke() {
+        println("start2")
+        delay(1000)
+        println("end2")
+    }
 }
 
 fun main() = runBlocking<Unit> {
-    val usecase = UseCase.instance
-    usecase.invoke(handler)
-    yield()
-    delay(250)
-    usecase.cancel()
-    val usecase2 = UseCase.instance
-    usecase2.invoke(handler)
-    yield()
-    delay(250)
-    usecase2.cancel()
+    try {
+        launch {
+            val usecase = Scenario(Usecase1(), Usecase2())
+            val usecase2 = Scenario(Usecase1(), Usecase2())
+            launch { usecase.invoke() }
+            launch { usecase2.invoke() }
+        }
+        delay(10)
+        cancel()
+    } catch (throwable: Throwable) {
+        println(throwable.message)
+    }
 }
